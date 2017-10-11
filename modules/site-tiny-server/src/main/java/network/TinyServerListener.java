0334843a73edde1c49efb78cf859db4862d9a9e9
Bo Cai <charpty@gmail.com>
2017-10-11 22:09:50 +0800

    修改：tiny服务器日常5分钟编码

--- modules/site-tiny-server/src/main/java/network/TinyServerListener.java
+++ modules/site-tiny-server/src/main/java/network/TinyServerListener.java
@@ -2,13 +2,15 @@ package network;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
-import boot.BootOption;
+import java.util.concurrent.Executors;
+import server.ServiceThreadPool;
 
 /**
  * @author caibo
@@ -17,8 +19,10 @@ import boot.BootOption;
  */
 public class TinyServerListener {
 
-	public TinyServerListener(int port) {
+	private final ServiceThreadPool serviceThreadPool;
 
+	public TinyServerListener(int port, ServiceThreadPool serviceThreadPool) {
+		this.serviceThreadPool = serviceThreadPool;
 	}
 
 	public void init(int port) throws IOException {
@@ -32,9 +36,17 @@ public class TinyServerListener {
 		Iterator<SelectionKey> it = keys.iterator();
 		while (it.hasNext()) {
 			SelectionKey key = it.next();
-			if ((key.readyOps() & SelectionKey.OP_ACCEPT) == 1) {
-				// TODO Service Thread
+			if (key.isAcceptable()) {
+				// 有连接建立开始处理，这样是不是大量连接建立销毁？
+				// 专门处理HTTP的服务器是这种处理办法吗？
+				ServerSocketChannel server = (ServerSocketChannel) key.channel();
+				SocketChannel channel = server.accept();
+				channel.configureBlocking(false);
+				channel.register(selector, SelectionKey.OP_READ);
+			} else if (key.isReadable()) {
+				// TODO 在这里读取数据并处理
 			}
+			it.remove();
 		}
 	}
 
--- modules/site-tiny-server/src/main/java/server/ServiceThreadPool.java
+++ modules/site-tiny-server/src/main/java/server/ServiceThreadPool.java
@@ -1,5 +1,6 @@
 package server;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionHandler;
@@ -13,6 +14,20 @@ import java.util.concurrent.TimeUnit;
  * @since 2017/10/10 下午9:14
  */
 public class ServiceThreadPool extends ThreadPoolExecutor {
+
+	public ServiceThreadPool() {
+		super(10, 30, 1800, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(64), new ThreadFactory() {
+			@Override
+			public Thread newThread(Runnable r) {
+				return new Thread(r);
+			}
+		}, new RejectedExecutionHandler() {
+			@Override
+			public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+				throw new RuntimeException("queue is full");
+			}
+		});
+	}
 
 	public ServiceThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue,
 			ThreadFactory threadFactory, RejectedExecutionHandler handler) {
