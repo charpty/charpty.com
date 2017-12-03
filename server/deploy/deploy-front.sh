7e03036c942bef76b96c7c1621703abdc66891ec
charpty <charpty@gmail.com>
2017-12-03 21:07:17 +0800

    修改：将vendor.js放置在oss上

--- server/deploy/deploy-front.sh
+++ server/deploy/deploy-front.sh
@@ -9,6 +9,11 @@ git pull origin master:master
 
 cd modules/site-front/app
 npm run build
+
+# 由于vendor.js偏大（1M左右），服务器带宽却非常少，所以将其放在OSS上
+# TODO 目前仅有caibo.ren有证书
+sed 's/\/static\/js\/vendor.*\.js/https:\/\/caibo.ren\/s\/vendor.js/' index.html
+
 # 仅仅将html目录setfacl给site用户
 rm -rf ~/html-bak
 mv /usr/local/openresty/nginx/html/* ~/html-bak/ || true
