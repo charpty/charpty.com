f7f321b870a4bd6e2d9a2b1674bcef80ebdb4d35
charpty <charpty@gmail.com>
2017-12-03 22:10:20 +0800

    修改：自动将js文件上传到OSS

--- server/deploy/deploy-front.sh
+++ server/deploy/deploy-front.sh
@@ -10,13 +10,17 @@ git pull origin master:master
 cd modules/site-front/app
 npm run build
 
+cd ..
 # 由于vendor.js偏大（1M左右），服务器带宽却非常少，所以将其放在OSS上
 # TODO 目前仅有caibo.ren有证书
-sed -ibakvendorjs 's/\/static\/js\/vendor.*\.js/https:\/\/caibo.ren\/s\/vendor.js/' index.html
+sed -ibakvendorjs 's/\/static\/js\/vendor.*\.js/https:\/\/caibo.ren\/s\/vendor.js/' dist/index.html
 rm -rf ./bakvendorjs
+cp dist/static/js/vendor*.js vendor.js
+python upload_imags_alioss.py vendor.js
 
 # 仅仅将html目录setfacl给site用户
 rm -rf ~/html-bak
+mkdir -p ~/html-bak
 mv /usr/local/openresty/nginx/html/* ~/html-bak/ || true
 cp -rf dist/* /usr/local/openresty/nginx/html/
 
