71a3abb5f737e1eddf1460d2dcb8744e738466cb
charpty <charpty@gmail.com>
2017-12-03 22:35:22 +0800

    修改：规范脚本采用绝对路径

--- server/deploy/deploy-front.sh
+++ server/deploy/deploy-front.sh
@@ -4,25 +4,31 @@
 
 set -e
 
-cd charpty.com
+base_dir=~/charpty.com
+front_dir=${base_dir}/modules/site-front
+compile_dir=${front_dir}/app
+dist_dir=${front_dir}/dist
+
+cd $base_dir
 git pull origin master:master
 
-cd modules/site-front/app
+cd $compile_dir
 npm run build
 
-cd ..
 # 由于vendor.js偏大（1M左右），服务器带宽却非常少，所以将其放在OSS上
 # TODO 目前仅有caibo.ren有证书
-sed -ibakvendorjs 's/\/static\/js\/vendor.*\.js/https:\/\/caibo.ren\/s\/vendor.js/' dist/index.html
+cd ~
+sed -ibakvendorjs 's/\/static\/js\/vendor.*\.js/https:\/\/caibo.ren\/s\/vendor.js/' ${dist_dir}/index.html
 rm -rf ./bakvendorjs
-cp dist/static/js/vendor*.js vendor.js
+cp ${dist_dir}/static/js/vendor*.js ~/vendor.js
+cd ~
 python upload_s.py vendor.js
 
 # 仅仅将html目录setfacl给site用户
 rm -rf ~/html-bak
 mkdir -p ~/html-bak
 mv /usr/local/openresty/nginx/html/* ~/html-bak/ || true
-cp -rf dist/* /usr/local/openresty/nginx/html/
+cp -rf ${dist_dir}/* /usr/local/openresty/nginx/html/
 
 # clean cache files
 # TODO
