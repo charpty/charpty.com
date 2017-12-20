e60fb643ddf536edd461cbba32393ddfcd245da5
charpty <charpty@gmail.com>
2017-12-20 22:17:04 +0800

    修改：HTTP解析测试例

--- modules/site-tiny-server/src/test/java/network/TinyServerListenerTest.java
+++ modules/site-tiny-server/src/test/java/network/TinyServerListenerTest.java
@@ -3,12 +3,14 @@ package network;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
+import sun.nio.ch.DirectBuffer;
 
 /**
  * @author caibo
@@ -24,35 +26,61 @@ public class TinyServerListenerTest {
 		Selector selector = Selector.open();
 		// 写事件注册，持续可写时可以攻击客户端
 		ssc.register(selector, SelectionKey.OP_ACCEPT);
+		outer:
 		while (true) {
+			long t1 = System.nanoTime();
 			int select = selector.select();
 			Set<SelectionKey> keys = selector.selectedKeys();
 			Iterator<SelectionKey> it = keys.iterator();
 			while (it.hasNext()) {
 				SelectionKey next = it.next();
 				// 并发删除测试
-				it.remove();
 				if (next.isAcceptable()) {
 					ServerSocketChannel channel = (ServerSocketChannel) next.channel();
 					SocketChannel socket = channel.accept();
 					socket.configureBlocking(false);
 					socket.register(selector, SelectionKey.OP_READ);
+					it.remove();
 				} else if (next.isReadable()) {
 					// ab测试10W
 					SocketChannel channel = (SocketChannel) next.channel();
-					ByteBuffer bb = ByteBuffer.allocate(128);
+					ByteBuffer bb = (ByteBuffer) next.attachment();
+					if (bb == null) {
+						bb = ByteBuffer.allocate(128);
+						next.attach(bb);
+					}
 					int read = channel.read(bb);
-					byte[] arr = new byte[read];
+					if (read < 0) {
+						continue;
+					}
+					// 扩容测试
+					if (read == 0 && bb.position() == bb.capacity()) {
+						ByteBuffer tmp = ByteBuffer.allocate(bb.capacity() * 2);
+						tmp.put(bb);
+						bb = tmp;
+						next.attach(bb);
+						continue;
+					}
+					String tmp = new String(bb.array(), 0, bb.position());
+					System.out.println(tmp);
+					if (tmp.lastIndexOf("\r\n\r\n") < 0 && tmp.lastIndexOf("\n\n") < 0) {
+						continue;
+					}
+					// byte[] arr = new byte[read];
 					// 大量读与溢出测试
-					System.arraycopy(bb.array(), 0, arr, 0, read);
-					System.out.println(new String(arr));
+					// System.arraycopy(bb.array(), 0, arr, 0, read);
+					// System.out.println(new String(arr));
+
 					// 尾部换行
 					bb.clear();
-					bb.put(arr);
+					String res = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 4" + "\r\n\r\n123456789" + "\r\n\r\n\r\n";
+					bb.put(res.getBytes());
 					bb.flip();
 					channel.write(bb);
+					channel.close();
+					it.remove();
 				}
-
+				System.out.println("处理" + select + "个耗时:" + (System.nanoTime() - t1));
 			}
 		}
 	}
