3f806cb20a6b29eda978cc806e455142ca3677aa
charpty <charpty@gmail.com>
2019-01-09 19:22:05 +0800

    增加：更新脚本

--- server/deploy/deploy-front.sh
+++ server/deploy/deploy-front.sh
@@ -4,15 +4,16 @@
 
 set -e
 
+work_dir=$(dirname "$(readlink -f "$0")")
 base_dir=~/charpty.com
 front_dir=$base_dir/modules/site-front
 dist_dir=$front_dir/dist
-html_bak=/tmp/.charpt-site-html-bak
+html_bak=/tmp/.charpty-site-html-bak
 
 cd $base_dir
 git pull origin master:master
 
-sh compile-front.sh $base_dir
+sh $work_dir/compile-front.sh $base_dir
 
 rm -rf $html_bak
 mkdir -p $html_bak
@@ -20,5 +21,5 @@ mv /usr/local/openresty/nginx/html/* $html_bak/ || true
 cp -rf $dist_dir/* /usr/local/openresty/nginx/html/
 
 # dc is an alias: for destroy the nginx cache and restart nginx
-dc
+
 echo '\033[0;32m*******************deploy success***************************'
---
+++ server/deploy/update_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd charpty.com
+git pull origin
+cd ..
+cp charpty.com/server/deploy/* .
